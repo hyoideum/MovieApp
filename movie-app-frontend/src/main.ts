65ff34e267d8eca291d2e35a1c16e3140a3a473a
hyoideum <ivana.protic@ymail.com>
2025-11-10 12:52:30 +0100

    checking token exp

--- movie-app-frontend/src/main.ts
+++ movie-app-frontend/src/main.ts
@@ -4,10 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './app/interceptors/auth.interceptor';
+import { AuthService } from './app/services/auth.service';
 
 bootstrapApplication(App, {
   providers: [
     provideRouter(routes), 
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    AuthService
     ],
 }).catch((err) => console.error(err));
